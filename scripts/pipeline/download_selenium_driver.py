e13417b2aed1d133332d825197767b6ee9627694
kpotoh <axepon@mail.ru>
2022-02-14 13:08:38 +0200

    try added to downloading

--- scripts/pipeline/download_selenium_driver.py
+++ scripts/pipeline/download_selenium_driver.py
@@ -1,4 +1,8 @@
+import sys
 from step_1_browser import setup_browser
 
 if __name__ == "__main__":
-    setup_browser(True)
+    try:
+        setup_browser(True)
+    except:
+        print("Script fell down, but it doesn't matter")
